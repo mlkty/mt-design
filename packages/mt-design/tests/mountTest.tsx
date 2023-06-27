fcc5ec752cca7b8931eae4ebc61133e05df248a9
liujinghan <liujinghan@baidu.com>
2023-06-27 15:35:28 +0800

    chore: 增加 styleslint 配置 so-kno-7809
    Change-Id: I3affc779b989c8702d116a1b534f845b083e563c

--- packages/mt-design/tests/mountTest.tsx
+++ packages/mt-design/tests/mountTest.tsx
@@ -1,13 +1,13 @@
 import { render } from './utils';
 
 export default function mountTest(Component: React.ComponentType<any>) {
-  describe('mount and unmount', () => {
-    it('component could be updated and unmounted without errors', () => {
-      const { unmount, rerender } = render(<Component />);
-      expect(() => {
-        rerender(<Component />);
-        unmount();
-      }).not.toThrow();
+    describe('mount and unmount', () => {
+        it('component could be updated and unmounted without errors', () => {
+            const { unmount, rerender } = render(<Component />);
+            expect(() => {
+                rerender(<Component />);
+                unmount();
+            }).not.toThrow();
+        });
     });
-  });
 }
